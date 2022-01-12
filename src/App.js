f1d8086d3b74c48bb8a56361ebbb5d1143f0e9d7
Manyu Lakhotia <lakemonyew@gmail.com>
2022-01-12 15:35:38 -0600

    Updates resume section
    Simplifies getDateRangeFormatted conditions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Home from "./components";
-import Resume from "./components/Resume";
-import Navbar from "./components/Navbar";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Home from './components';
+import Resume from './components/Resume';
+import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
       <CssBaseline />
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/resume" component={Resume} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/resume' component={Resume} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/contact' component={Contact} />
       </Switch>
     </React.Fragment>
   );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,234 +1,176 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { join } from 'path';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   mainContainer: {
-    background: "#233",
+    background: '#233',
   },
   timeLine: {
-    position: "relative",
-    padding: "1rem",
-    margin: "o auto",
-    "&:before": {
+    'position': 'relative',
+    'padding': '1rem',
+    'margin': 'o auto',
+    '&:before': {
       content: "''",
-      position: "absolute",
-      height: "100%",
-      border: "1px solid tan",
-      right: "40px",
+      position: 'absolute',
+      height: '100%',
+      border: '1px solid tan',
+      right: '40px',
       top: 0,
     },
-    "&:after": {
+    '&:after': {
       content: "''",
-      display: "table",
-      clear: "both",
+      display: 'table',
+      clear: 'both',
     },
-    [theme.breakpoints.up("md")]: {
-      padding: "2rem",
-      "&:before": {
-        left: "calc(50% - 1px)",
-        right: "auto",
+    [theme.breakpoints.up('md')]: {
+      'padding': '2rem',
+      '&:before': {
+        left: 'calc(50% - 1px)',
+        right: 'auto',
       },
     },
   },
   timeLineItem: {
-    padding: "1rem",
-    borderBottom: "2px solid tan",
-    position: "relative",
-    margin: "1rem 3rem 1rem 1rem",
-    clear: "both",
-    "&:after": {
+    'padding': '1rem',
+    'borderBottom': '2px solid tan',
+    'position': 'relative',
+    'margin': '1rem 3rem 1rem 1rem',
+    'clear': 'both',
+    '&:after': {
       content: "''",
-      position: "absolute",
+      position: 'absolute',
     },
-    "&:before": {
+    '&:before': {
       content: "''",
-      position: "absolute",
-      right: "-0.625rem",
-      top: "calc(50% - 5px)",
-      borderStyle: "solid",
-      borderColor: "tomato tomato transparent transparent",
-      borderWidth: "0.625rem",
-      transform: "rotate(45deg)",
+      position: 'absolute',
+      right: '-0.625rem',
+      top: 'calc(50% - 5px)',
+      borderStyle: 'solid',
+      borderColor: 'tomato tomato transparent transparent',
+      borderWidth: '0.625rem',
+      transform: 'rotate(45deg)',
     },
-    [theme.breakpoints.up("md")]: {
-      width: "44%",
-      margin: "1rem",
-      "&:nth-of-type(2n)": {
-        float: "right",
-        margin: "1rem",
-        borderColor: "tan",
+    [theme.breakpoints.up('md')]: {
+      'width': '44%',
+      'margin': '1rem',
+      '&:nth-of-type(2n)': {
+        float: 'right',
+        margin: '1rem',
+        borderColor: 'tan',
       },
-      "&:nth-of-type(2n):before": {
-        right: "auto",
-        left: "-0.625rem",
-        borderColor: "transparent transparent tomato tomato",
+      '&:nth-of-type(2n):before': {
+        right: 'auto',
+        left: '-0.625rem',
+        borderColor: 'transparent transparent tomato tomato',
       },
     },
   },
   timeLineYear: {
-    textAlign: "center",
-    maxWidth: "9.375rem",
-    margin: "0 3rem 0 auto",
-    fontSize: "1.8rem",
-    color: "#fff",
-    background: "tomato",
-    lineHeight: 1,
-    padding: "0.5rem 1rem",
-    "&:before": {
-      display: "none",
+    'textAlign': 'center',
+    'maxWidth': '15.375rem',
+    'margin': '0 3rem 0 auto',
+    'fontSize': '1.8rem',
+    'color': '#fff',
+    'background': 'tomato',
+    'lineHeight': 1,
+    'padding': '0.5rem 1rem',
+    '&:before': {
+      display: 'none',
     },
-    [theme.breakpoints.up("md")]: {
-      textAlign: "center",
-      margin: "0 auto",
-      "&:nth-of-type(2n)": {
-        float: "none",
-        margin: "0 auto",
+    [theme.breakpoints.up('md')]: {
+      'textAlign': 'center',
+      'margin': '0 auto',
+      '&:nth-of-type(2n)': {
+        float: 'none',
+        margin: '0 auto',
       },
-      "&:nth-of-type(2n):before": {
-        display: "none",
+      '&:nth-of-type(2n):before': {
+        display: 'none',
       },
     },
   },
   heading: {
-    color: "tomato",
-    padding: "3rem 0",
-    textTransform: "uppercase",
+    color: 'tomato',
+    padding: '3rem 0',
+    textTransform: 'uppercase',
   },
   subHeading: {
-    color: "#fff",
+    color: '#fff',
     padding: 0,
-    textTransform: "uppercase",
+    textTransform: 'uppercase',
   },
   body1: {
-    color: "tomato",
+    color: 'tomato',
   },
   subtitle1: {
-    color: "tan",
+    color: 'tan',
+  },
+  subtitle2: {
+    color: 'tomato',
   },
 }));
 
-const Resume = () => {
+function getDateRangeFormated(dateRange) {
+  const beginYear = new Date(dateRange.beginDate).getFullYear();
+  var endYear = new Date(dateRange.endDate)?.getFullYear() ?? dateRange.endDate;
+  endYear = isNaN(endYear) ? dateRange.endDate : endYear;
+  return [beginYear === endYear, !endYear].some(b => b) ? beginYear : `${beginYear} - ${endYear}`;
+}
+
+const WorkExpEntry = ({ job }) => {
   const classes = useStyles();
   return (
-    <Box component="header" className={classes.mainContainer}>
-      <Typography variant="h4" align="center" className={classes.heading}>
+    <React.Fragment>
+      <Typography variant='h2' className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+        {job.workDateRanges.map(getDateRangeFormated).join(' and ')}
+      </Typography>
+      <Box component='div' className={classes.timeLineItem}>
+        <Typography variant='h5' align='center' className={classes.subHeading}>
+          {job.title}
+        </Typography>
+        <Typography variant='body1' align='center' className={classes.body1}>
+          {job.company}
+        </Typography>
+        <Typography variant='subtitle1' align='center' className={classes.subtitle1}>
+          {job.points?.map(point => point.text).join('. ')}
+        </Typography>
+        <Typography variant='subtitle2' align='center' className={classes.subtitle2}>
+          {job.timeDedicated}
+          {job?.teamSize ? ` - Team size of ${job.teamSize}` : ''} - {job.location}
+        </Typography>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+const Resume = () => {
+  const classes = useStyles();
+  const [resumeData, setResumeData] = React.useState();
+
+  React.useEffect(() => {
+    (async () => {
+      setResumeData(
+        await fetch(join(__dirname, 'resume_data.json'), {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        }).then(r => r.json())
+      );
+    })();
+  }, []);
+
+  return (
+    <Box component='header' className={classes.mainContainer}>
+      <Typography variant='h4' align='center' className={classes.heading}>
         Working Experience
       </Typography>
-      <Box component="div" className={classes.timeLine}>
-        <Typography
-          variant="h2"
-          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
-        >
-          2013
-        </Typography>
-        <Box component="div" className={classes.timeLineItem}>
-          <Typography
-            variant="h5"
-            align="center"
-            className={classes.subHeading}
-          >
-            web design
-          </Typography>
-          <Typography variant="body1" align="center" className={classes.body1}>
-            company name where worked
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            className={classes.subtitle1}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
-            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
-            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
-            repudiandae, rerum necessitatibus nisi mollitia.
-          </Typography>
-        </Box>
-        <Typography
-          variant="h2"
-          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
-        >
-          2014
-        </Typography>
-        <Box component="div" className={classes.timeLineItem}>
-          <Typography
-            variant="h5"
-            align="center"
-            className={classes.subHeading}
-          >
-            html & css
-          </Typography>
-          <Typography variant="body1" align="center" className={classes.body1}>
-            company name where worked
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            className={classes.subtitle1}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
-            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
-            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
-            repudiandae, rerum necessitatibus nisi mollitia.
-          </Typography>
-        </Box>
-        <Typography
-          variant="h2"
-          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
-        >
-          2015
-        </Typography>
-        <Box component="div" className={classes.timeLineItem}>
-          <Typography
-            variant="h5"
-            align="center"
-            className={classes.subHeading}
-          >
-            Fullstack Javascript
-          </Typography>
-          <Typography variant="body1" align="center" className={classes.body1}>
-            company name where worked
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            className={classes.subtitle1}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
-            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
-            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
-            repudiandae, rerum necessitatibus nisi mollitia.
-          </Typography>
-        </Box>
-        <Typography
-          variant="h2"
-          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
-        >
-          2017
-        </Typography>
-        <Box component="div" className={classes.timeLineItem}>
-          <Typography
-            variant="h5"
-            align="center"
-            className={classes.subHeading}
-          >
-            Django & React
-          </Typography>
-          <Typography variant="body1" align="center" className={classes.body1}>
-            company name where worked
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            className={classes.subtitle1}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
-            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
-            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
-            repudiandae, rerum necessitatibus nisi mollitia.
-          </Typography>
-        </Box>
+
+      <Box component='div' className={classes.timeLine}>
+        {resumeData && resumeData.workExperience.jobs.map((job, i) => <WorkExpEntry key={i} job={job} />)}
       </Box>
     </Box>
   );
