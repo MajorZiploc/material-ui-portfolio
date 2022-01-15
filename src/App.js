8ee5179b0e340901f343cf526d4f5f5504e400c7
Manyu Lakhotia <lakemonyew@gmail.com>
2022-01-15 17:41:57 -0600

    Adds error boundary to App component

--- src/App.js
+++ src/App.js
@@ -6,20 +6,38 @@ import Resume from './components/Resume';
 import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import './App.css';
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role='alert'>
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/resume' component={Resume} />
-        <Route exact path='/portfolio' component={Portfolio} />
-        <Route exact path='/contact' component={Contact} />
-      </Switch>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          // reset the state of your app so the error doesn't happen again
+        }}
+      >
+        <CssBaseline />
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/resume' component={Resume} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+      </ErrorBoundary>
     </React.Fragment>
   );
 }
