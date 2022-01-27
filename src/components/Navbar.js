337e289026b20a2ea13e8466a2006e0f066e8107
Manyu Lakhotia <lakemonyew@gmail.com>
2022-01-27 08:16:52 -0600

    Changes right arrow to hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-import ArrowBack from '@material-ui/icons/ArrowBack';
+import MenuIcon from '@material-ui/icons/Menu';
 import AssignmentInd from '@material-ui/icons/AssignmentInd';
 import Home from '@material-ui/icons/Home';
 import Apps from '@material-ui/icons/Apps';
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
     background: '#222',
     margin: 0,
   },
-  arrow: {
+  hamburger: {
     color: 'tomato',
   },
   title: {
@@ -89,7 +89,7 @@ const Navbar = () => {
         <AppBar position='static' className={classes.appbar}>
           <Toolbar>
             <IconButton onClick={() => setOpen(true)}>
-              <ArrowBack className={classes.arrow} />
+              <MenuIcon className={classes.hamburger} />
             </IconButton>
             <Typography variant='h5' className={classes.title}>
               Portfolio
