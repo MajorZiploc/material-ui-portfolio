ccb1d2b23efc98bed4b5a724b4e44b5fd1cd7caf
Manyu Lakhotia <LakeMonYew@gmail.com>
2022-03-26 17:30:22 -0500

    Revert "rn resume{_data=>}"
    This reverts commit 82d547b57f16110a0ee4940f08f945ee7dbf91b6.

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 class Data {
   constructor() {
-    this.resumeData = fetch(`${process.env.PUBLIC_URL}/resume.json`, {
+    this.resumeData = fetch(`${process.env.PUBLIC_URL}/resume_data.json`, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
